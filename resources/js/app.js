f9964b3f371d08c099584d07bfb2a4022708e441
Sofia <sperezhidd@gmail.com>
2025-02-21 21:01:12 -0300

    feature add main sidebar hover effect

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+const sidebarEl = document.querySelector('.main-sidebar')
+
+sidebarEl.addEventListener('mouseenter', function() {
+    sidebarEl.classList.add('active')
+})
+
+sidebarEl.addEventListener('mouseleave', function() {
+    sidebarEl.classList.remove('active')
+})
