352ec525628ba02cddaef98e9a72bbccc832535b
Sofia <sperezhidd@gmail.com>
2025-03-03 01:15:13 -0300

    feature add route "new-purchase-order"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
-    <a href="/new-purchase-order"
-        class="change-oc-stage block w-fit rounded-[0.375rem] bg-[#0F172A] px-4 py-2 text-white">
+    <a href="/new-purchase-order" class="block w-fit rounded-[0.375rem] bg-[#0F172A] px-4 py-2 text-white">
         Cargar nueva orden de compra
     </a>
 
