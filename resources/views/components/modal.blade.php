4060c1cf0c61bb2fa2c685d03b1fef6c6cdc8319
Sofia <sperezhidd@gmail.com>
2025-03-06 09:22:37 -0300

    update modal background

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -48,7 +48,7 @@
     </div>
 
     <div x-show="show"
-        class="{{ $maxWidth }} mb-6 transform overflow-hidden rounded-lg bg-white p-8 shadow-xl transition-all sm:mx-auto sm:w-full"
+        class="{{ $maxWidth }} mb-6 transform overflow-hidden rounded-lg bg-[#ECECEC] p-8 shadow-xl transition-all sm:mx-auto sm:w-full"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200"
