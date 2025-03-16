f397dc5c83e106d6657985598321c440a5237283
Sofia <sperezhidd@gmail.com>
2025-03-15 23:31:53 -0300

    feature add Password view

---
+++ resources/views/livewire/settings/password.blade.php
@@ -0,0 +1,39 @@
+<form class="space-y-6 rounded-2xl bg-white px-6 py-4">
+    <div class="space-y-2">
+        <label for="language-select" class="flex flex-col">
+            <span class="text-lg font-bold text-[#7288FF]">Contraseña Actual</span>
+        </label>
+        <input type="password" class="text-lg font-bold text-[#7288FF] rounded-xl w-full focus:border-2 focus:border-[#7288FF] transition-all duration-300 border-2 border-[#D2D2D2]">
+    </div>
+
+    <div class="space-y-2">
+        <label for="language-select" class="flex flex-col">
+            <span class="text-lg font-bold text-[#7288FF] mb-2">
+                Nueva Contraseña
+            </span>
+
+            <span class="text-sm text-[#C2C2C2]">
+                Debe contener al menos 8 caracteres, incluyendo una letra mayúscula y un carácter especial
+            </span>
+        </label>
+        <input type="password" class="text-lg font-bold text-[#7288FF] rounded-xl w-full focus:border-2 focus:border-[#7288FF] transition-all duration-300 border-2 border-[#D2D2D2]">
+    </div>
+
+    <div class="space-y-2">
+        <label for="language-select" class="flex flex-col">
+            <span class="text-lg font-bold text-[#7288FF] mb-2">
+                Nueva Contraseña
+            </span>
+
+            <span class="text-sm text-[#C2C2C2]">
+                Debe contener al menos 8 caracteres, incluyendo una letra mayúscula y un carácter especial
+            </span>
+        </label>
+        <input type="password" class="text-lg font-bold text-[#7288FF] rounded-xl w-full focus:border-2 focus:border-[#7288FF] transition-all duration-300 border-2 border-[#D2D2D2]">
+    </div>
+
+    <button type="submit"
+        class="w-full rounded-[0.375rem] bg-[#565AFF] px-3 py-[0.625rem] text-center font-sans text-lg font-extrabold text-white transition-colors duration-500 hover:bg-[#7477ff] hover:text-slate-300 active:scale-[0.99]">
+        Guardar
+    </button>
+</form>
