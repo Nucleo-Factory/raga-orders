fcb9ac515f3497a2542b625ffb12327046c0dd02
Sofia <sperezhidd@gmail.com>
2025-03-17 03:04:17 -0300

    feature add sidebar dropdown component

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,10 +11,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const togglerBtn = sidebarEl.querySelector(".sidebar-toggler-btn");
     const profileContainer = sidebarEl.querySelector(".profile-container");
     const profileName = sidebarEl.querySelector(".profile-name");
+    const sidebarDropdown = sidebarEl.querySelector('.sidebar-dropdown')
+    const sidebarDropdownTextContainer = sidebarEl.querySelector(
+        ".sidebar-dropdown-text-container"
+    );
+    const sidebarDropdownText = sidebarEl.querySelector(
+        ".sidebar-dropdown-text"
+    );
+    const sidebarDropdownArrowIcon = sidebarEl.querySelector('.sidebar-dropdown .icon');
 
     let sidebarExpanded = false;
 
     function expandSidebar() {
+        sidebarEl.classList.add("sidebar-expanded");
         sidebarLinkAll.forEach((link) => {
             link.style.gap = "0.75rem";
         });
@@ -33,9 +42,22 @@ document.addEventListener("DOMContentLoaded", function () {
         profileContainer.style.gap = "0.625rem";
         profileName.style.width = profileName.scrollWidth + "px";
         profileName.style.opacity = "1";
+
+        sidebarDropdown.style.gap = "0.75rem"
+
+        sidebarDropdownTextContainer.style.gap = "0.625rem";
+
+        sidebarDropdownText.style.width =
+            sidebarDropdownText.scrollWidth + "px";
+        sidebarDropdownText.style.opacity = "1";
+
+        sidebarDropdownArrowIcon.style.width = sidebarDropdownArrowIcon.scrollWidth + "px";
+        sidebarDropdownArrowIcon.style.opacity = "1"
     }
 
     function collapseSidebar() {
+        sidebarEl.classList.remove("sidebar-expanded");
+
         sidebarLinkAll.forEach((link) => {
             link.style.gap = "0";
         });
@@ -54,6 +76,16 @@ document.addEventListener("DOMContentLoaded", function () {
         profileContainer.style.gap = "0";
         profileName.style.width = "0";
         profileName.style.opacity = "0";
+
+        sidebarDropdown.style.gap = "0"
+
+        sidebarDropdownTextContainer.style.gap = "0";
+
+        sidebarDropdownText.style.width = "0";
+        sidebarDropdownText.style.opacity = "0";
+
+        sidebarDropdownArrowIcon.style.width = "0";
+        sidebarDropdownArrowIcon.style.opacity = "0"
     }
 
     togglerBtn.addEventListener("click", function () {
