3652beb653d228e2e790720772ec728c98e87725
Sofia <sperezhidd@gmail.com>
2025-03-20 00:52:29 -0300

    feature add /support route

--- routes/web.php
+++ routes/web.php
@@ -107,4 +107,8 @@ Route::middleware(['auth'])->group(function () {
         ->name('settings.role-edit');
 });
 
+Route::view('support', 'support.index')
+    ->middleware(['auth'])
+    ->name('support.index');
+
 require __DIR__ . '/auth.php';
