79d8f187b5762e396e7054672e118ebe21f9aedc
Sofia <sperezhidd@gmail.com>
2025-03-19 22:40:26 -0300

    feature add /roles/{roleId}/edit view and controller

---
+++ app/Livewire/Settings/RoleEdit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Settings;
+
+use Livewire\Component;
+
+class RoleEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.settings.role-edit')
+            ->layout('layouts.settings.user-management');
+    }
+}
