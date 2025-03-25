5357e737b3f87b9dc014a7f559a5a400d6389773
Sofia <sperezhidd@gmail.com>
2025-03-24 23:07:06 -0300

    feature modify view-title content to render conditionally

--- resources/views/components/view-title.blade.php
+++ resources/views/components/view-title.blade.php
@@ -1,11 +1,13 @@
-@props(['title', 'content'])
+@props(['title', 'content' => null])
 
 <div>
     <h1 {{ $title->attributes->merge(['class' => 'text-[3.25rem] font-black leading-[3.75rem]']) }}>
         {{ $title }}
     </h1>
 
-    <p {{ $content->attributes->merge(['class' => 'text-lg']) }}>
-        {{ $content }}
-    </p>
+    @if ($content)
+        <p {{ $content->attributes->merge(['class' => 'text-lg']) }}>
+            {{ $content }}
+        </p>
+    @endif
 </div>
