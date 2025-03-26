da58daf6d30f5f9d5097d3e9dede51a80f6e263c
Sofia <sperezhidd@gmail.com>
2025-03-26 16:58:27 -0300

    feature add /settings/users view and controller

---
+++ app/Livewire/Settings/Users.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Settings;
+
+use Livewire\Component;
+
+class Users extends Component
+{
+    public function render()
+    {
+        return view('livewire.settings.users')
+            ->layout('layouts.settings.user-management');
+    }
+}
