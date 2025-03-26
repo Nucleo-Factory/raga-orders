984f112943bbc385c00b6691563fd6530d9f9619
Sofia <sperezhidd@gmail.com>
2025-03-26 17:03:02 -0300

    feature add /settings/active-sessions view and controller

---
+++ resources/views/livewire/settings/active-sessions.blade.php
@@ -0,0 +1,7 @@
+<div class="space-y-10 rounded-2xl bg-white p-8">
+    <div class="flex items-center justify-between">
+        <h2 class="text-lg font-bold text-[#7288FF]">Sesiones activas</h2>
+    </div>
+
+    {{-- Lista --}}
+</div>
