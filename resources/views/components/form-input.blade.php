badeb9d91a00840b00698f76bedb7a6f92f20efd
Sofia <sperezhidd@gmail.com>
2025-03-26 08:02:04 -0300

    feature add conditional render to label

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,10 +1,12 @@
-@props(['label', 'input', 'icon' => false])
+@props(['label' => null, 'input', 'icon' => null])
 
-<div {{$attributes->merge(['class' => 'flex flex-col'])}}>
-    <label @if ($input->attributes->has('name')) for="{{ $input->attributes->get('name') }}" @endif
-        {{ $label->attributes->merge(['class' => 'ml-[1.125rem] text-sm font-medium text-[#565AFF]']) }}>
-        {{ $label }}
-    </label>
+<div {{ $attributes->merge(['class' => 'flex flex-col']) }}>
+    @if ($label)
+        <label @if ($input->attributes->has('name')) for="{{ $input->attributes->get('name') }}" @endif
+            {{ $label->attributes->merge(['class' => 'ml-[1.125rem] text-sm font-medium text-[#565AFF]']) }}>
+            {{ $label }}
+        </label>
+    @endif
 
     @if ($icon)
         <div class="relative">
