812f390cee6cfe0584cce0ab89fc229512f6ca30
Sofia <sperezhidd@gmail.com>
2025-03-26 07:09:10 -0300

    feature update moda-success styles and structure

--- resources/views/components/modal-success.blade.php
+++ resources/views/components/modal-success.blade.php
@@ -1,31 +1,17 @@
-@props(['title' => '', 'show' => false, 'maxWidth' => 'lg'])
+@props(['title' => null, 'show' => false, 'maxWidth' => 'lg'])
 
-<x-modal name="success-modal" maxWidth="{{ $maxWidth }}" show="{{ $show }}" {{ $attributes }}>
-    <div class="mb-4 flex">
-        <h3 class="ml-auto text-lg font-bold text-success">
-            Operación Exitosa
+<x-modal maxWidth="{{ $maxWidth }}" show="{{ $show }}" {{ $attributes }}>
+    <div class="mb-4 flex flex-col items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" width="94" height="93" viewBox="0 0 94 93" fill="none">
+            <path
+                d="M27.5001 46.5013L40.5001 59.5013L66.5001 33.5013M90.3334 46.5013C90.3334 70.4336 70.9324 89.8346 47.0001 89.8346C23.0677 89.8346 3.66675 70.4336 3.66675 46.5013C3.66675 22.569 23.0677 3.16797 47.0001 3.16797C70.9324 3.16797 90.3334 22.569 90.3334 46.5013Z"
+                stroke="#5DD595" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+
+        <h3 class="text-lg font-bold text-success">
+            {{ $title ?? 'Operación Exitosa' }}
         </h3>
-
-        <button type="button" aria-label="Cerrar" x-data=""
-            x-on:click="$dispatch('close-modal', 'success-modal')" class="ml-auto">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#A5A3A3">
-                <path fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd" />
-            </svg>
-        </button>
     </div>
 
-    <div class="mb-8 space-y-2">
-        {{ $slot }}
-
-        <span class="text-center text-sm text-[#AFAFAF] block">
-            La operación se encuentra pendiente de aprobación por parte de su supervisor
-        </span>
-    </div>
-
-    <x-primary-button class="w-full" x-data=""
-    x-on:click="$dispatch('close-modal', 'success-modal')">
-        Aceptar
-    </x-primary-button>
+    {{ $slot }}
 </x-modal>
