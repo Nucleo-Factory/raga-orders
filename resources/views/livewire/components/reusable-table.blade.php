fef88468eba82732c9afdf1ce9d5cbeb5203c0dd
Cristian Quiroz <cristian.quiroz@nucleofactory.clo>
2025-06-15 22:27:28 -0400

    Update reusable-table.blade.php

--- resources/views/livewire/components/reusable-table.blade.php
+++ resources/views/livewire/components/reusable-table.blade.php
@@ -10,7 +10,7 @@
                         wire:model.live="search"
                         class="rounded-xl border-2 border-[#A5A3A3] pl-11 pr-[1.125rem] py-[0.625rem] placeholder:text-[#9AABFF] w-full" />
                     <div class="pointer-events-none absolute top-1/2 -translate-y-1/2 left-[1.125rem] flex items-center">
-                        <svg class="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -181,6 +181,8 @@
                                                     {{ $row->$key }}
                                                 @elseif(is_object($row->$key))
                                                     {{ method_exists($row->$key, '__toString') ? $row->$key : $row->$key->id }}
+                                                @elseif(strtolower($key) === 'estado' && strtolower($row->$key) === 'active')
+                                                    activo
                                                 @else
                                                     {{ $row->$key }}
                                                 @endif
@@ -218,6 +220,8 @@
                                                     @endforeach
                                                 @elseif(isset($row[$key . '_formatted']))
                                                     {!! $row[$key . '_formatted'] !!}
+                                                @elseif(strtolower($key) === 'estado' && strtolower($row[$key]) === 'active')
+                                                    activo
                                                 @else
                                                     {{ $row[$key] }}
                                                 @endif
