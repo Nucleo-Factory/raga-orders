4611b1bdeecf667092636325154d9de71e9f7043
Cristian Quiroz <cristian.quiroz@nucleofactory.clo>
2025-08-12 21:18:53 -0400

    Remove Raga Orders PO Confirmation module: Deleted all related files including configuration, views, routes, and service provider. Updated .gitignore to exclude internal modules. Cleaned up composer.json and composer.lock to reflect the removal of the module.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
+    App\Providers\ModuleServiceProvider::class,
+    // RagaOrders\POConfirmation\POConfirmationServiceProvider::class, // Ahora se maneja desde ModuleServiceProvider
 ];
